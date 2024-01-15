534ea18b54678a3817b3e98dded05438dd9f9eae
Andrew <andrew.fei@ossii.com.tw>
2024-01-15 18:27:48 +0800

    新增 package.json 、修復webpack.js

--- webpack.js
+++ webpack.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -9,5 +11,32 @@ module.exports = {
 		publicPath: '/js/',
 		filename: '[name].js',
 		chunkFilename: 'files.[id].js'
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		}
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new CleanWebpackPlugin(),
+	],
+	devtool: 'source-map',
 }
